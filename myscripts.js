864f0ef5b7b0b59b142fdd885a33bcff2c92b19e
Deepimpact7 <deepimpact7@protonmail.com>
2023-04-05 15:21:51 +0200

    Create the function that plays a round

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,42 @@
 const getComputerChoice = Math.floor(Math.random() * 3) + 1;
-const getUserChoice = prompt("Choose between Rock, Paper, Scissor");
-console.log(getUserChoice)
+const getUserChoice = prompt("Choose between Rock, Paper, Scissors");
+console.log("User = " + getUserChoice)
 
 if (getComputerChoice === 1) {
-    console.log("Rock");
+    console.log("Computer = Rock");
 } else if (getComputerChoice === 2) {
-    console.log("Paper");
+    console.log("Computer = Paper");
+} else if (getComputerChoice === 3){
+    console.log("Computer = Scissors");
 } else {
-    console.log("Scissor");
+    console.log("There is a problem!");
 }
+
+const computerSelection = getComputerChoice;
+const playerSelection = getUserChoice;
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === 1 && playerSelection === "Rock") {
+        return alert("You both choosen Rock");
+    } else if (computerSelection === 1 && playerSelection === "Paper") {
+        return alert("You Win! Paper beats Rock")
+    } else if (computerSelection === 1 && playerSelection === "Scissors") {
+        return alert("You Lose! Rock beats Scissors")
+    } else if (computerSelection === 2 && playerSelection === "Rock") {
+        return alert("You Lose! Paper beats Rock")
+    } else if (computerSelection === 2 && playerSelection === "Paper") {
+        return alert("You both choosen Paper")
+    }  else if (computerSelection === 2 && playerSelection === "Scissors") {
+        return alert("You Win! Scissors beats Paper")
+    } else if (computerSelection === 3 && playerSelection === "Rock") {
+        return alert("You Win! Rock beats Scissors")
+    }  else if (computerSelection === 3 && playerSelection === "Paper") {
+        return alert("You Lose! Scissors beats Paper")
+    }  else if (computerSelection === 3 && playerSelection === "Scissors") {
+        return alert("You both choosen Scissors")
+    } else {
+        return alert("There is a problem");
+    }
+}
+
+playRound(playerSelection, getComputerChoice)
