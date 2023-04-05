38f006aa5560c6933aad3999fb37a9b7c7c8c2b1
Deepimpact7 <deepimpact7@protonmail.com>
2023-04-05 12:18:08 +0200

    Let the PC decide Rock-Paper-Scissor

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+if (randomNumber === 1) {
+    console.log("Rock");
+} else if (randomNumber === 2) {
+    console.log("Paper");
+} else {
+    console.log("Scissor");
+}
